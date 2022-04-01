ca20d8490597a359bb9c6cf42be0862b42343f19
chrismartel <christian.martel2@mail.mcgill.ca>
2022-04-01 09:55:14 -0400

    removed unused dir

--- dataset/download_dataset.sh
+++
@@ -1,6 +0,0 @@
-# Script to download project dataset from onedrive link. 
-
-#!/bin/bash
-wget -O output https://mcgill-my.sharepoint.com/:u:/g/personal/raghav_mehta_mail_mcgill_ca/EVEvhY9_jyVEk2uSZ8wZhFYBQ58C57I7ZB55jBocKwB5Jg?download=1
-mv output dataset.zip
-unzip dataset.zip

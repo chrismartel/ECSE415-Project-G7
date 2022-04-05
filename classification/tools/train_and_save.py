07c7a9611b7368e36a4dae39eebf622bbea29b43
chrismartel <christian.martel2@mail.mcgill.ca>
2022-04-04 20:22:06 -0400

    cleaner train and save

--- classification/tools/train_and_save.py
+++ classification/tools/train_and_save.py
@@ -1,33 +1,40 @@
 import pickle
 import json
 import yaml
+import gc
 
-# Train Dataset parameters
-positive_negative_ratio=1
-number_of_positive_samples = 2000
-sequences_positive_negative_ratio = 0.1
-number_of_positive_samples_per_sequence = 50
+# Dataset parameters
 min_intersection_ratio = 0.8
-train_sequences = ['0','1','2']
+number_of_positive_samples = 4000
+number_of_negative_samples = 6000
+number_of_positive_samples_per_sequence = 1000
+number_of_negative_samples_per_sequence = 3000
 
 # Preprocessing parameters
-input_shape = (64,64)
-orientations = 20 # number of orientation bins
-pixels_per_cell = (2,2) # number of pixels per cell
-cells_per_block = (4,4) # number of cells per block
-compute_spatial_features = True
-spatial_bins = (16,16)
+resize_shape = (64,64)
+orientations = 9 # number of orientation bins
+pixels_per_cell = (8,8) # number of pixels per cell
+cells_per_block = (2,2) # number of cells per block
+compute_spatial_features=False
+spatial_bins =(8,8)
+
+classifier_type = 'linear'
 
 # SVM parameters
-classifier_type = 'linear'
-C = 10
+C = 1000
 bagging = False
 n_estimators = 1
 
+# Random Forest params
+n_estimators_random_forest = 500
+criterion = 'entropy'
+max_depth = None
+min_samples_split = 2
 
-visualize = False
+sequence_ids = ['0','1','2']
 
-def train_and_save_model_v2(path, save_model=True, save_config=True, train_sequences=train_sequences,classifier_type=classifier_type, number_of_positive_samples_per_sequence=number_of_positive_samples_per_sequence, sequences_positive_negative_ratio=sequences_positive_negative_ratio, positive_negative_ratio=positive_negative_ratio, min_intersection_ratio=min_intersection_ratio, number_of_positive_samples=number_of_positive_samples, input_shape=input_shape, compute_spatial_features=compute_spatial_features, orientations=orientations, pixels_per_cell=pixels_per_cell,cells_per_block=cells_per_block,C=C, bagging=bagging, n_estimators=n_estimators):
+
+def train_and_save_model(path, save_model=True, save_config=True, train_sequences=sequence_ids,classifier_type=classifier_type, number_of_positive_samples=number_of_positive_samples, number_of_negative_samples=number_of_negative_samples, number_of_positive_samples_per_sequence=number_of_positive_samples_per_sequence, number_of_negative_samples_per_sequence=number_of_negative_samples_per_sequence, min_intersection_ratio=min_intersection_ratio, resize_shape=resize_shape, compute_spatial_features=compute_spatial_features, spatial_bins=spatial_bins, orientations=orientations, pixels_per_cell=pixels_per_cell,cells_per_block=cells_per_block,C=C, bagging=bagging, n_estimators=n_estimators, n_estimators_random_forest=n_estimators_random_forest, criterion=criterion, max_depth=max_depth, min_samples_split=min_samples_split):
   '''
     Train a svm classifier with HoG features and save it.
 
@@ -37,12 +44,12 @@ def train_and_save_model_v2(path, save_model=True, save_config=True, train_seque
   '''
 
   # Build Dataset
-  imgs, labels = build_dataset_v2(positive_negative_ratio=positive_negative_ratio, number_of_positive_samples=number_of_positive_samples)
-  sequences_imgs, sequences_labels = build_dataset_from_sequences(train_sequences,positive_negative_ratio=sequences_positive_negative_ratio,min_intersection_ratio=min_intersection_ratio, number_of_positive_samples=number_of_positive_samples_per_sequence, resize_shape=input_shape, visualize=visualize)
-  imgs, labels = np.concatenate((imgs,sequences_imgs), axis=0), np.concatenate((labels,sequences_labels), axis=0)
+  imgs, labels = build_dataset_from_udacity(number_of_positive_samples=number_of_positive_samples, number_of_negative_samples=number_of_negative_samples, visualize=False)
+  seq_imgs, seq_labels = build_dataset_from_sequences(sequence_ids,min_intersection_ratio=min_intersection_ratio, number_of_positive_samples_per_sequence=number_of_positive_samples_per_sequence, number_of_negative_samples_per_sequence=number_of_negative_samples_per_sequence, visualize=False)
+  imgs, labels = imgs + seq_imgs, np.concatenate((labels,seq_labels), axis=0)
 
   # Preprocess
-  features = preprocess_images_v2(imgs, orientations, pixels_per_cell, cells_per_block, compute_spatial_features=compute_spatial_features, spatial_bins=spatial_bins)
+  features = preprocess_images(imgs, orientations=orientations, resize_shape=resize_shape, pixels_per_cell=pixels_per_cell, cells_per_block=cells_per_block, scaling=True, preprocessing_time=False, compute_spatial_features=False, spatial_bins=spatial_bins, visualize=False, grayscale=True)
 
   # Shuffle
   number_of_samples = features.shape[0]
@@ -51,48 +58,55 @@ def train_and_save_model_v2(path, save_model=True, save_config=True, train_seque
 
   x_train, y_train = features[inds], labels[inds]
 
+  # LINEAR SVM
   if classifier_type == 'linear':
     # Bagging classifier
     if bagging:
-      model = BaggingClassifier(base_estimator=svm.LinearSVC(C=C), n_estimators=n_estimators, random_state=None)
-
-    # SVM Binary Classifier
+      clf = BaggingClassifier(base_estimator=svm.SVC(gamma='scale', C=C), n_estimators=n_estimators, random_state=None)
+    # Binary Classifier
     else:
-      model = svm.LinearSVC(C=C)
+      clf = svm.SVC(gamma='scale', C=C)
+  
+  # RBF SVM
   elif classifier_type == 'rbf':
     # Bagging classifier
     if bagging:
-      model = BaggingClassifier(base_estimator=svm.SVC(gamma='scale',C=C), n_estimators=n_estimators, random_state=None)
-
-    # SVM Binary Classifier
+      clf = BaggingClassifier(base_estimator=svm.LinearSVC(C=C), n_estimators=n_estimators, random_state=None)
+    # Binary Classifier
     else:
-      model = svm.SVC(gamma='scale',C=C)
+      clf = svm.LinearSVC(C=C)
+
+  # Random Forest
+  elif classifier_type == 'rf':
+    # Bagging classifier
+    if bagging:
+      clf = BaggingClassifier(base_estimator=RandomForestClassifier(n_estimators=n_estimators_random_forest, criterion=criterion), n_estimators=n_estimators, random_state=None)
+    # Random Forest Classifier
+    else:
+      clf = RandomForestClassifier(n_estimators=n_estimators_random_forest, criterion=criterion)
 
   # Train
-  model.fit(x_train, y_train)
+  clf.fit(x_train, y_train)
 
   # Save model
   if save_model:
-    pickle.dump(model,open(path+'.sav', 'wb'))
+    pickle.dump(clf,open(path+'.sav', 'wb'))
 
   # Save model hyperparameters
-
-# Sequences -> image sequences to use to train
-train_sequences = ['0','1','2']
   if save_config:
     data = dict()
     data['dataset'] = dict()
     data['preprocessing'] = dict()
-    data['svm'] = dict()
+    data['clf'] = dict()
 
-    data['dataset']['positive_negative_ratio'] = positive_negative_ratio
     data['dataset']['min_intersection_ratio'] = min_intersection_ratio
     data['dataset']['number_of_positive_samples'] = number_of_positive_samples
+    data['dataset']['number_of_negative_samples'] = number_of_negative_samples
     data['dataset']['number_of_positive_samples_per_sequence'] = number_of_positive_samples_per_sequence
-    data['dataset']['sequences_positive_negative_ratio'] = sequences_positive_negative_ratio
+    data['dataset']['number_of_negative_samples_per_sequence'] = number_of_negative_samples_per_sequence
     
-    data['preprocessing']['input_shape'] = dict()
-    data['preprocessing']['input_shape']['y'], data['preprocessing']['input_shape']['x'] = input_shape[0], input_shape[1]
+    data['preprocessing']['resize_shape'] = dict()
+    data['preprocessing']['resize_shape']['y'], data['preprocessing']['resize_shape']['x'] = input_shape[0], input_shape[1]
     data['preprocessing']['orientations'] = orientations
     data['preprocessing']['compute_spatial_features'] = compute_spatial_features
     data['preprocessing']['spatial_bins'] = dict()
@@ -111,4 +125,11 @@ train_sequences = ['0','1','2']
     with open(path+'.yaml', "w") as outfile:
       yaml.dump(data, outfile)
 
-  return model
+    del imgs
+    del features
+    del seq_imgs
+    del seq_labels
+    del labels
+    gc.collect()
+
+  return clf
